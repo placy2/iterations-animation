cfac935328ec19ae0aaefa58ac4e7165d4ed61cb
Parker <placy@trinity.edu>
2020-11-08 02:35:45 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from "super-tiny-icons/images/svg/github.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <body>
+      <div className="credits">
+        <p>Original CSS animations inspired by <a href="https://codepen.io/alja9/pen/jgPVEm" target="_blank">This Codepen.</a></p>
+      </div>
+      <div className="bio">
+        <p>Parker Lacy, 2020.
+          <a href="https://www.github.com/placy2" target="_blank">
+            <img src={logo} width="24px" height="24px"></img>
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        
+      </div>
+      <div className="container">
+        <div className="loader">
+          <div className="x _1" style={{'--index': 9}}></div>
+          <div className="x _2" style={{'--index': 8}}></div>
+          <div className="x _3" style={{'--index': 7}}></div>
+          <div className="x _4" style={{'--index': 6}}></div>
+          <div className="x _5" style={{'--index': 5}}></div>
+          <div className="x _6" style={{'--index': 4}}></div>
+          <div className="x _7" style={{'--index': 3}}></div>
+          <div className="x _8" style={{'--index': 2}}></div>
+          <div className="x _9" style={{'--index': 1}}></div>
+          <div className="x _10" style={{'--index': 0}}></div>
+        </div>
+      </div>
+    </body>
   );
 }
 
