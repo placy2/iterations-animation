200f8cf611ac61254f41accb240d120a53a8f86c
Parker <placy@trinity.edu>
2020-11-08 03:05:24 -0600

    Fix warnings and restyle top text boxes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import './App.css';
-import logo from "super-tiny-icons/images/svg/github.svg";
+import gitLogo from "super-tiny-icons/images/svg/github.svg";
+import linkedLogo from "super-tiny-icons/images/svg/linkedin.svg";
 
 function App() {
   return (
-    <body>
+    <div className="body-div">
       <div className="credits">
-        <p>Original CSS animations inspired by <a href="https://codepen.io/alja9/pen/jgPVEm" target="_blank">This Codepen.</a></p>
-      </div>
-      <div className="bio">
-        <p>Parker Lacy, 2020.
-          <a href="https://www.github.com/placy2" target="_blank">
-            <img src={logo} width="24px" height="24px"></img>
+        <p>Original CSS animations inspired by 
+          <a href="https://codepen.io/alja9/pen/jgPVEm" target="_blank" rel="noreferrer">
+            This Codepen.
           </a>
         </p>
-        
+      </div>
+      <div className="bio">
+        <a className="gitlogo" href="https://www.github.com/placy2" target="_blank" rel="noreferrer">
+          <img src={gitLogo} width="36px" height="36px" alt="Github logo"></img>
+        </a>
+        <a className="linkedinLogo" href="https://www.linkedin.com/in/parker-lacy-2021" target="_blank" rel="noreferrer">
+          <img src={linkedLogo} width="36px" height ="36px" alt="LinkedIn logo"></img>
+        </a>
+        <p>Parker Lacy, 2020</p>
       </div>
       <div className="container">
         <div className="loader">
@@ -29,7 +35,7 @@ function App() {
           <div className="x _10" style={{'--index': 0}}></div>
         </div>
       </div>
-    </body>
+    </div>
   );
 }
 
